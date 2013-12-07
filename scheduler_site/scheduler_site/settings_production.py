3946694ee31932266a93464a77b69ef977803800
Sefa Kilic <sefakilic@gmail.com>
2013-12-07 13:20:20 -0500

    scheduler.css

--- scheduler_site/scheduler_site/settings_production.py
+++ scheduler_site/scheduler_site/settings_production.py
@@ -75,6 +75,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "schedule_display"),
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), "static"),
 )
 
 # List of finder classes that know how to find static files in
