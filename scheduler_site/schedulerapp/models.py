d8904ce9d6650aa2f0a8e846f7e6be9234f8b3b6
Sefa <sefakilic@gmail.com>
2013-12-06 23:51:22 -0500

    1st 'database done' commit message

--- scheduler_site/schedulerapp/models.py
+++ scheduler_site/schedulerapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Session(models.Model):
-    DAY_OF_THE_WEEK = (
+    DAYS_OF_THE_WEEK = (
         ('Mon', 'Monday'),
         ('Tue', 'Tuesday'),
         ('Wed', 'Wednesday'),
@@ -12,10 +12,54 @@ class Session(models.Model):
         ('Sat', 'Saturday'),
         ('Sun', 'Sunday'),
     )
-    day_of_the_week = models.CharField(max_length=3, choices=DAY_OF_THE_WEEK)
+    day_of_the_week = models.CharField(max_length=50, choices=DAYS_OF_THE_WEEK)
     time_start_hr = models.IntegerField()
     time_start_min = models.IntegerField()
     time_end_hr = models.IntegerField()
     time_end_min = models.IntegerField()
     room = models.CharField(max_length=50)
+    section = models.ForeignKey(Section)
+
+class Course(models.Model):
+    COURSE_TYPES = (
+        ('lab', 'lab'),
+        ('lecture', 'lecture')
+    )
+    dept = models.CharField(max_length=50)
+    number = models.IntegerField() # course number
+    title = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    description = models.TextField()
+    prerequisites = models.ManyToMany(Course)
+    corequsites = models.ManyToMany(Course)
+
+class Section(models.Model):
+    SEMESTERS = (
+        ('fall', 'Fall'),
+        ('winter', 'Winter'),
+        ('spring', 'Spring'),
+        ('summer', 'Summer'),
+    )
+    course_id = models.IntegerField() # section-specific course id
+    professor = models.ManyToManyField(Professor)
+    semester = models.CharField(max_length=50, choices=SEMESTERS)
+    year = models.IntegerField()
+    evaluation = models.OneToOneField(Evaluation)
+
+class Evaluation(models.Model):
+    pass
+
+class Professor(models.Model):
+    name = models.CharField(max_length=128)
     
+class Major(models.Model):
+    courses = models.ManyToManyField(Course)
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=256)
+    sections = models.ManyToManyField(Section)
+    friends = models.ManyToManyField(Users)
+    majors = models.ManyToManyField(Major)
+
