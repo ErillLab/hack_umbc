14e78763b17a2acbdd0e8ef989ec9d68d0995e85
Patrick O'Neill <synapse.and.syntax@gmail.com>
2013-12-07 04:43:25 -0500

    updated utils

--- src/sceq_miner/utils.py
+++ src/sceq_miner/utils.py
@@ -4,3 +4,15 @@ def partition_according_to(f,xs):
     xsys = [(x,f(x)) for x in xs]
     yvals = set([y for (x,y) in xsys])
     return [[x for (x,y) in xsys if y == yval] for yval in yvals]
+
+def concat(xxs):
+    return [x for xs in xxs for x in xs]
+
+def h(ps):
+    """compute entropy (in bits) of a probability distribution ps"""
+    return -sum([p * safe_log2(p) for p in ps])
+
+def normalize(xs):
+    total = float(sum(xs))
+    return [x/total for x in xs]
+
